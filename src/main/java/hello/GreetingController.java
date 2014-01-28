94e852661fc3eca8f568d2da68e614bf2cad048b
jychoi <jychoi@jychoiui-Mac-mini.local>
2014-01-28 15:09:15 +0900

    Initial Commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+	
+	private static final String template = "Hello, $s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/gretting")
+	public @ResponseBody Gretting gretting(
+			@RequestParam(value="name", required=false, defaultValue="World") String name
+			){
+		return new Gretting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
+
